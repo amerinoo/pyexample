bab549a67f2a10a95b08b945a30e2cc8b6b3462e
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2021-09-01 17:35:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 def main():
     print("Starting main")
     while True:
-        print("Hello World updated again!")
+        print("Hello World updated again x2!")
         time.sleep(5)
 
 if __name__ == "__main__":
